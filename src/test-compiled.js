de48f0e79d155e77a287691ea216f198644b04f8
king-king <niuyiwangqun@sohu.com>
2016-11-18 22:20:47 +0800

    c

--- src/test-compiled.js
+++ src/test-compiled.js
@@ -6,6 +6,8 @@
 function test() {
   var a = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 32;
   var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 50;
+
+  console.log(a, b);
 }
 
 //# sourceMappingURL=test-compiled.js.map
